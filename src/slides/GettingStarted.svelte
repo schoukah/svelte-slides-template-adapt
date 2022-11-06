b984ddecef8438c1dc69dbff78e74f7b2237775f
Sarah Choukah <21149759+schoukah@users.noreply.github.com>
2022-11-06 08:00:16 -0500

    Initial commit

---
+++ src/slides/GettingStarted.svelte
@@ -0,0 +1,14 @@
+<script>
+ import Notes from '../lib/Notes.svelte';
+
+</script>
+
+<section>
+    <h2>Getting started</h2>
+    <ul>
+        <li class="fragment fade-up">Configure your Presentation in <code>config.js</code></li>
+        <li class="fragment fade-up">Create your Slides in <code>src/slides</code></li>
+        <li class="fragment fade-up">Add your Slides in <code>Presentation.svelte</code></li>
+    </ul>
+		<Notes>This is an example of slide notes</Notes>
+</section>
